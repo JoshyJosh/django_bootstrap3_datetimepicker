72e05c0885b235fe5724767eec93aec5e34ce0c9
JoshyJosh <herukula@gmail.com>
2017-02-27 22:16:27 +0100

    added tests for event modelž

--- calendar_events/tests.py
+++ calendar_events/tests.py
@@ -7,10 +7,18 @@ from .models import Event
 
 # Create your tests here.
 
+
 class EventSlugTest(TestCase):
 	def setUp(self):
 		Event.objects.create(content="Some test content", title="Some Title", event_time=timezone.now())
-		
-	def slug_is_created_without_explicit_input(self):
+		Event.objects.create(content="Different content", title="Some Title", event_time=timezone.now())
+
+	def test_slug_is_created_without_explicit_input(self):
 		ev = Event.objects.get(content="Some test content")
-		self.assertEqual(ev.slug, "some_content")
+		self.assertEqual(ev.slug, "some-title")
+
+	def test_slug_is_created_and_incremented_if_already_used(self):
+		ev1 = Event.objects.get(content="Some test content")
+		ev2 = Event.objects.get(content="Different content")
+		self.assertEqual(ev1.slug, "some-title")
+		self.assertEqual(ev2.slug, "some-title_1")
